f6f402feeb8173646f41bf6e9a6b1572691737b7
BERKER <101291907+Xerkerr@users.noreply.github.com>
2022-11-26 21:15:36 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    //maas
     public static void main(String[] args) {
         Employee employee1 = new Employee("kemal",2000.0,45,1985);
         System.out.println(employee1);
